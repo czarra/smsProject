199cf86661e2e763e2b4aa139638cdcf1d16ee92
Radek <radomich11@gmail.com>
2019-07-07 01:20:11 +0200

    read from file txt

--- app/src/main/java/com/example/rad/smsproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/rad/smsproject/Activity/MainActivity.java
@@ -1,60 +1,135 @@
 package com.example.rad.smsproject.Activity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rad.smsproject.R;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button sendSMS;
-    public EditText smsText;
-    private String[] smsNumbers = {
+    private Button sendSMS, openFile;
+    private EditText smsText;
+    private TextView fileName;
+    private Uri selectedfile;
+    private ProgressBar progressBar;
+    private File file;
+//    private String[] smsNumbers = {
+//
+//    };
+    private ArrayList<String> smsNumbers = new ArrayList<String>();
 
-    };
     private String basicMessage = System.getProperty("line.separator")+"Send by My App";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission();
-
+        progressBar= (ProgressBar) findViewById(R.id.progressBar);
         sendSMS = (Button) findViewById(R.id.sendSMS);
         smsText = (EditText) findViewById(R.id.smsText);
+        openFile = (Button) findViewById(R.id.openFile);
+        fileName = (TextView) findViewById(R.id.fileName);
+
         sendSMS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if( !smsText.getText().toString().isEmpty()) {
-                    for (String number : smsNumbers) {
-                        try {
-                            if (checkPermission()) {
-                                sendSMS(number, smsText.getText().toString()+basicMessage);
+                if(file != null  && getExtension(file).equals("txt")) {
+                    sendSMS.setVisibility(View.INVISIBLE);
+                    openFile.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.VISIBLE);
+                    String textToSend = smsText.getText().toString();
+                    if (!smsText.getText().toString().isEmpty()) {
+                        setSmsNumbers(file);
+                        if(!smsNumbers.isEmpty()) {
+                            for (String number : smsNumbers) {
+                                Log.e("number", number);
+                                try {
+                                    if (checkPermission()) {
+                                        sendSMS(number,textToSend  + basicMessage);
+                                    }
+                                } catch (android.content.ActivityNotFoundException ex) {
+                                    Toast.makeText(MainActivity.this, "Can't send SMS", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        } catch (android.content.ActivityNotFoundException ex) {
-                            Toast.makeText(MainActivity.this, "Can't send SMS", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Empty phone list", Toast.LENGTH_SHORT).show();
                         }
+                    } else {
+                        Toast.makeText(MainActivity.this, "Message can't be empty!!!", Toast.LENGTH_SHORT).show();
                     }
+                    sendSMS.setVisibility(View.VISIBLE);
+                    openFile.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.GONE);
                 } else {
-                    Toast.makeText(MainActivity.this, "Message can't be empty!!!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Please choose a file (.txt)!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+        openFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    // start runtime permission
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                            PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},99);
+                    } else {
+                        Log.e("ss", "get permision-- already granted ");
+                        showFileChooser();
+                    }
+                }else {
+                    //readfile();
+                    showFileChooser();
                 }
 
 
             }
         });
+    }
+
+    private void showFileChooser(){
+        try {
+            Intent intent = new Intent()
+                    .setType("*/*")
+                    .setAction(Intent.ACTION_GET_CONTENT);
+
+            startActivityForResult(Intent.createChooser(intent, "Select a file"), 123);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "Can't open File", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private boolean checkPermission(){
@@ -66,6 +141,78 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    private String getPath(Uri uri){
+        String toRemove = "/document/raw:";
+        return uri.getPath().replace(toRemove, "");
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 123 && resultCode == RESULT_OK) {
+            selectedfile = data.getData(); //The uri with the location of the file
+            Log.e("a", getPath(selectedfile));//.getPath());
+            file = new File( getPath(selectedfile));
+            if(file.exists()) {
+                String extension = getExtension(file);
+                if (extension.equals("txt")) {
+                    fileName.setText(file.getName());
+                    fileName.setVisibility(View.VISIBLE);
+                } else {
+                    fileName.setVisibility(View.INVISIBLE);
+                    file = null;
+                }
+            } else {
+                Log.e("error","file not exist");
+                fileName.setVisibility(View.INVISIBLE);
+                file = null;
+            }
+        }
+    }
+
+    /*
+     * Get the extension of a file.
+     */
+    private static String getExtension(File f) {
+        String ext = "";
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 && i < s.length() - 1) {
+            ext = s.substring(i + 1).toLowerCase();
+        }
+        Log.e("ext",ext);
+        return ext;
+    }
+
+    private void setSmsNumbers(File f){
+        smsNumbers =  new ArrayList<String>();
+        //
+        try{
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            String[] items ;
+            while ((line = br.readLine()) != null) {
+                items = line.split(";");
+                for (String item : items)
+                {
+                    smsNumbers.add(item);
+                }
+
+            }
+            br.close();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+            Log.e("error","can't read "+e.getMessage());
+
+        }
+
     }
 
     private void sendSMS(String number, String message){
